775521ab7afd32a003f43bf7096d09e2eb33602b
Yedam <waterclean101@gmail.com>
2022-10-23 18:44:16 +0900

    1023

---
+++ back/src/back.java
@@ -0,0 +1,7 @@
+public class back {
+    public static void main(String[] args) {
+        System.out.println("Why is JAVA so hard?");
+        System.out.println("I just wanna use Python(cry)..");
+        System.out.println("But it's always fun to learn a new language, hihi.");
+    }
+}
